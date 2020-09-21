f7143842c19505b90ba232027e7d0664e56ef766
Arjun Ramesh <69367261+theArj@users.noreply.github.com>
2020-09-21 16:11:52 -0700

    Displayed moods with button using javascript

---
+++ resources/js/scripts.js
@@ -0,0 +1,33 @@
+const itemsContainer = document.getElementById("items");
+
+import data from "./data.js";
+
+// the length of our data determines how many times this loop goes around
+for (let i=0; i<data.length; ++i) {
+    let newDiv = document.createElement('div');
+      newDiv.className = 'item'
+    // display the image
+    let img = document.createElement('img');
+    img.src = data[i].image
+    img.width = 300
+    img.height = 300
+    newDiv.appendChild(img)
+  
+    let desc = document.createElement('P')
+    desc.innerText =data[i].desc
+    newDiv.appendChild(desc)
+    let price = document.createElement('P')
+    price.innerText = data[i].price
+    newDiv.appendChild(price)
+  
+    let button = document.createElement('button')
+    button.id = data[i].name
+  
+    // creates a custom attribute called data-price.
+    // That will hold the price for each element in the button
+    button.dataset.price = data[i].price
+    button.innerHTML = "Add to Cart"
+    newDiv.appendChild(button)
+    // put new div inside items container
+    itemsContainer.appendChild(newDiv)
+  }
