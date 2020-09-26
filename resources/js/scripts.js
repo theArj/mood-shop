c99c17c82e9defae97335486fa28cbb78b0a1142
Arjun Ramesh <69367261+theArj@users.noreply.github.com>
2020-09-26 15:20:31 -0700

    displayed cart items on console

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -34,16 +34,49 @@ for (let i=0; i<data.length; ++i) {
 
   const = cart []
 
+  // Add Item.
   function addItem(name, price, qty) {
-    const item = {name: name, price: price, qty: 1}
+    for (let i = 0; i < cart.length; i += 1) {
+      if (cart[i].name === name) {
+        cart[i].qty += 1
+        // Stop running code here with return statement if duplicates are added to cart.
+        return
+      }
+    }
+    const item = {name, price, qty: 1}
     cart.push(item)
   }
 
+  // Show Items.
   function showItems() {
-    console.log(`You have ${cart.length} items in your cart.`)
+    const qty = getQty()
+    console.log(`You have ${qty} items in your cart.`)
+    for (let i = 0; i < cart.length; i += 1) {
+      console.log(`- ${cart[i].name} $${cart[i].price} x ${cart[i].qty}`)
+    }
+
+    console.log(`Total in cart: $${getTotal()}`)
   }
 
-  addItem("Apple", 0.99, 3)
-  addItem("Orange", 1.29, 4)
+  // Get Quantity.
+  function getQty() {
+    let qty = 0
+    for (let i = 0; i < cart.length; i += 1) {
+      qty += cart[i].qty
+    }
+    return qty
+  }
+
+  // Get Total.
+  function getTotal() {
+    let total = 0
+    for (let i = 0; i < cart.length; i += 1) {
+      total += cart[i].price * cart[i].qty
+    }
+    return total.toFixed(2)
+  }
+
+  // addItem("Apple", 0.99, 3)
+  // addItem("Orange", 1.29, 4)
 
   showItems()
